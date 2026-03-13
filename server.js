5468de4036097d27e7893aa61b8663b0e6f3c2fd
Imed-Zairi <dev1@renaji.app>
2026-03-13 04:04:14 +0100

    Add CORS configuration

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const swaggerSpec = require('./swagger');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
