b95065c568aed2e9459247cf165d96aa5b2e98ec
Imed-Zairi <dev1@renaji.app>
2026-03-13 04:10:50 +0100

    Add Supabase headers to CORS config

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Prefer, apikey, x-client-info');
   
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
